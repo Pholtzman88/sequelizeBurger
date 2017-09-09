39d34cabe2c1c3e864113ccaad2d7a10319d5dbb
Pholtzman88 <holtzman.patrick@gmail.com>
2017-09-08 19:36:49 -0500

    fixing config

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,6 @@ var Sequelize = require("Sequelize");
 var connection;
 if (process.env.JAWSDB_URL) {
 	connection = new Sequelize(process.env.JAWSDB_URL);
-	connection.connect();
 }else{
 	connection = new Sequelize("sqlburgers_db", "root", "", {
 	  host: "localhost",
